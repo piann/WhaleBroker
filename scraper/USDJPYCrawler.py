5182c675aa9f4ef2dd34efeb5ccda51b792b1fb2
DongSu <piann@DongSus-iMac.local>
2019-07-29 15:51:47 +0900

    inherit done : USDCPYCrawler

--- scraper/USDJPYCrawler.py
+++ scraper/USDJPYCrawler.py
@@ -1,7 +1,24 @@
+from ..common import *
 from .base import *
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+
+
 
 class USDJPYCrawler(InvestingCrawler):
     
     def __init__(self):
         super().__init__()
-        
+        self.formData = {"curr_id": 3,
+            "smlID": 106684,
+            "header": "USD/JPY Historical Data",
+            "st_date": "",
+            "end_date": "",
+            "interval_sec": "Daily",
+            "sort_col": "date",
+            "sort_ord": "DESC",
+            "action": "historical_data"
+        }
+        self.isAmountExist = False
